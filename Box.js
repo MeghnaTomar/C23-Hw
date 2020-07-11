44e095e5d08e5185e5e706de68d8ce4bb4c87dbd
MeghnaTomar <66015551+MeghnaTomar@users.noreply.github.com>
2020-07-11 20:01:50 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,19 @@
+class Box {
+    constructor(x,y,width,height){
+        var options ={
+            'restitution':0,
+            'density':1
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("red");
+        rect(0,0,this.width,this.height);
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# C23-Hw
-Homework Assignment
+# SupplyMission
